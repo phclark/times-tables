f6781d6bfb7d317085e33bb189c1325f28a57332
Philip Clark <philip.alan.clark@gmail.com>
2025-01-05 17:53:14 -0500

    print layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [number, setNumber] = useState(100);
@@ -10,7 +10,7 @@ export default function Home() {
 
   const generateTable = () => {
     const newTable = [];
-    for (let i = 0; i <= rows - 1; i++) {
+    for (let i = 0; i < rows; i++) {
       const row = [];
       for (let j = 1; j <= columns; j++) {
         const numString = (i * number * rows + j * number).toLocaleString(
@@ -23,11 +23,19 @@ export default function Home() {
     setTable(newTable);
   };
 
+  useEffect(() => {
+    generateTable();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="min-h-screen p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 items-center">
-        <h1 className="text-2xl font-bold">Multiplication Table Generator</h1>
-        <div className="flex flex-col md:flex-row gap-4 items-center">
+        <h1 className="text-2xl font-bold no-print">
+          Multiplication Table Generator
+        </h1>
+        <h1 className="text-2xl font-bold print-only">{number} Times Table</h1>
+        <div className="flex flex-col md:flex-row gap-4 items-center no-print">
           <input
             type="number"
             value={number}
@@ -79,14 +87,19 @@ export default function Home() {
         </div>
         {table.length > 0 && (
           <div className="overflow-x-auto max-md:w-full">
-            <table className="border-collapse border border-gray-400 w-full">
+            <table
+              className="border-collapse border border-gray-400 w-full
+              rounded-lg
+              overflow-hidden
+              no-first-row-col-borders"
+            >
               <thead>
                 <tr>
-                  <th className="border border-gray-400 p-2 text-center"></th>
-                  {[...Array(columns).keys()].map((i) => (
+                  <th className="border border-gray-400 p-2 text-center bg-gray-800"></th>
+                  {[...Array(table[0]?.length).keys()].map((i) => (
                     <th
                       key={i}
-                      className="border border-gray-400 p-2 text-center"
+                      className="border border-gray-400 p-2 text-center font-light  bg-gray-800"
                     >
                       {i + 1}
                     </th>
@@ -96,13 +109,13 @@ export default function Home() {
               <tbody>
                 {table.map((row, rowIndex) => (
                   <tr key={rowIndex}>
-                    <td className="border border-gray-400 p-2 text-center">
+                    <td className="border border-gray-400 font-light p-2 text-center bg-gray-800">
                       {rowIndex + 1}
                     </td>
                     {row.map((cell, cellIndex) => (
                       <td
                         key={cellIndex}
-                        className="border border-gray-400 p-2 text-center"
+                        className={`border border-gray-400 p-2 text-center ${cellIndex % 2 === 0 ? "bg-gray-600" : "bg-gray-700"}`}
                       >
                         {cell}
                       </td>
